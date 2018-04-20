817cccf41630eee1c4015ad14bf526273a676998
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-20 16:58:07 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+ counter = 0
+ board.each do |turn|
+   if turn == "X" || turn == "O"
+     counter += 1
+ end
+end
+
+def current_player
+  if turn_count % 2 == 1
+    return "X"
+  else
+    return "O"
+end
